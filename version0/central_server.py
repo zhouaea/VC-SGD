0687ef5335308fb6fd8671a06e05c3e364adc82e
Neo Zhou <zhouaea@bc.edu>
2021-05-31 13:15:21 -0400

    Save current work with loss function

--- version0/central_server.py
+++ version0/central_server.py
@@ -1,20 +1,23 @@
+import time
+
 from neural_network import Neural_Network
 from vehicle import Vehicle
 from data_partition import data_for_polygon
 from data_partition import val_train_data, val_test_data
-from gluoncv import model_zoo
+from gluoncv.model_zoo import get_model
 import gluoncv as gcv
+from gluoncv.model_zoo.yolo.yolo_target import YOLOV3PrefetchTargetGenerator
 from gluoncv.utils.metrics.voc_detection import VOCMApMetric
 import numpy as np
 import yaml
 import mxnet as mx
-from mxnet import gluon, nd
+from mxnet import gluon, nd, autograd
 import csv
 import os
 
-
 file = open('config.yml', 'r')
 cfg = yaml.load(file, Loader=yaml.FullLoader)
+
 
 # np.random.seed(cfg['seed'])
 
@@ -25,23 +28,24 @@ class Central_Server:
     - model
     - accumulative_gradients
     """
+
     def __init__(self, ctx):
         self.net = gluon.nn.Sequential()
         if cfg['dataset'] == 'cifar10':
             with self.net.name_scope():
                 #  First convolutional layer
-                self.net.add(gluon.nn.Conv2D(channels=64, kernel_size=3, padding=(1,1), activation='relu'))
+                self.net.add(gluon.nn.Conv2D(channels=64, kernel_size=3, padding=(1, 1), activation='relu'))
                 self.net.add(gluon.nn.BatchNorm())
-                self.net.add(gluon.nn.Conv2D(channels=64, kernel_size=3, padding=(1,1), activation='relu'))
+                self.net.add(gluon.nn.Conv2D(channels=64, kernel_size=3, padding=(1, 1), activation='relu'))
                 self.net.add(gluon.nn.BatchNorm())
                 self.net.add(gluon.nn.MaxPool2D(pool_size=2, strides=2))
                 self.net.add(gluon.nn.Dropout(rate=0.25))
                 #  Second convolutional layer
                 # net.add(gluon.nn.MaxPool2D(pool_size=2, strides=2))
                 # Third convolutional layer
-                self.net.add(gluon.nn.Conv2D(channels=128, kernel_size=3, padding=(1,1), activation='relu'))
+                self.net.add(gluon.nn.Conv2D(channels=128, kernel_size=3, padding=(1, 1), activation='relu'))
                 self.net.add(gluon.nn.BatchNorm())
-                self.net.add(gluon.nn.Conv2D(channels=128, kernel_size=3, padding=(1,1), activation='relu'))
+                self.net.add(gluon.nn.Conv2D(channels=128, kernel_size=3, padding=(1, 1), activation='relu'))
                 self.net.add(gluon.nn.BatchNorm())
                 self.net.add(gluon.nn.MaxPool2D(pool_size=2, strides=2))
                 self.net.add(gluon.nn.Dropout(rate=0.25))
@@ -56,14 +60,14 @@ class Central_Server:
                 self.net.add(gluon.nn.Dense(128, activation="relu"))
                 # net.add(gluon.nn.Dense(256, activation="relu"))
                 self.net.add(gluon.nn.Dropout(rate=0.25))
-                self.net.add(gluon.nn.Dense(10)) # classes = 10
+                self.net.add(gluon.nn.Dense(10))  # classes = 10
         elif cfg['dataset'] == 'mnist':
             with self.net.name_scope():
                 self.net.add(gluon.nn.Dense(128, activation='relu'))
                 self.net.add(gluon.nn.Dense(64, activation='relu'))
                 self.net.add(gluon.nn.Dense(10))
         elif cfg['dataset'] == 'pascalvoc':
-            net = model_zoo.get_model('yolo3_darknet53_voc', pretrained_base=False)
+            self.net = get_model('yolo3_darknet53_voc', pretrained=False)
         self.net.initialize(mx.init.Xavier(), ctx=ctx, force_reinit=True)
 
         self.accumulative_gradients = []
@@ -80,7 +84,8 @@ class Central_Server:
                     # mapping back to the collection of ndarray
                     # directly update model
                     lr = cfg['neural_network']['learning_rate']
-                    param.set_data(param.data() - lr * mean_nd[idx:(idx+param.data().size)].reshape(param.data().shape))
+                    param.set_data(
+                        param.data() - lr * mean_nd[idx:(idx + param.data().size)].reshape(param.data().shape))
                     idx += param.data().size
             self.accumulative_gradients = []
 
@@ -94,7 +99,9 @@ class Simulation:
     - rsu_list
     - dataset
     """
-    def __init__(self, FCD_file, vehicle_dict: dict, rsu_list: list, vc_list: list, polygons, central_server, num_round):
+
+    def __init__(self, FCD_file, vehicle_dict: dict, rsu_list: list, vc_list: list, polygons, central_server,
+                 num_round):
         self.FCD_file = FCD_file
         self.vehicle_dict = vehicle_dict
         self.rsu_list = rsu_list
@@ -111,58 +118,133 @@ class Simulation:
         self.num_round = num_round
         self.running_time = 0
         if cfg['dataset'] == 'pascalvoc':
+            self.fake_x = mx.nd.zeros((cfg['neural_network']['batch_size'], 3, 416, 416))
+            with autograd.train_mode():
+                _, self.anchors, self.offsets, self.feat_maps, _, _, _, _ = self.central_server.net(self.fake_x)
+            self.target_generator = YOLOV3PrefetchTargetGenerator(
+                num_class=len(self.central_server.net.classes))
             self.epoch_loss = gcv.loss.YOLOV3Loss()
             self.epoch_accuracy = VOCMApMetric()
+            self.sum_losses = []
+            self.obj_losses = []
+            self.center_losses = []
+            self.scale_losses = []
+            self.cls_losses = []
         else:
             self.epoch_loss = mx.metric.CrossEntropy()
             self.epoch_accuracy = mx.metric.Accuracy()
-       
+
     def add_into_vehicle_dict(self, vehicle):
         self.vehicle_dict[vehicle.attrib['id']] = Vehicle(vehicle.attrib['id'])
 
     def get_accu_loss(self):
         # accuracy on testing data
+        start = time.time()
         for i, (data, label) in enumerate(self.val_test_data):
-            outputs = self.central_server.net(data)
-            # this following line takes EXTREMELY LONG to run
-            self.epoch_accuracy.update(label, outputs)
+            if cfg['dataset'] == 'pascalvoc':
+                outputs = self.central_server.net(data)
+                pred_object_class_indices = outputs[:][0]
+                pred_object_probabilities = outputs[:][1]
+                pred_bboxes = outputs[:][2]
+                gt_bboxes = label[:, :, 0:4]
+                gt_class_indices = label[:, :, 4:5]
+                self.epoch_accuracy.update(pred_bboxes, pred_object_class_indices, pred_object_probabilities, gt_bboxes,
+                                           gt_class_indices)
+                print("accuracy computed: ", self.epoch_accuracy.get())
+
+            else:
+                outputs = self.central_server.net(data)
+                # this following line takes EXTREMELY LONG to run
+                self.epoch_accuracy.update(label, outputs)
+        end = time.time()
+        print('time to calculate accuracy for 10 test data:', end-start)
+        start = time.time()
         # cross entropy on training data
         for i, (data, label) in enumerate(self.val_train_data):
-            outputs = self.central_server.net(data)
-            self.epoch_loss.update(label, nd.softmax(outputs))
+            if cfg['dataset'] == 'pascalvoc':
+                # Acquire all variables required to calculate loss.
+                gt_bboxes = mx.nd.array(label[:, :, :4]).astype(np.float32)
+                gt_ids = label[:, :, 4:5]
 
+                objectness, center_targets, scale_targets, weights, class_targets = self.target_generator(
+                    self.fake_x, self.feat_maps, self.anchors, self.offsets,
+                    gt_bboxes, gt_ids, None)
+
+                # Calculate loss by using network in recording AND training mode and supplying extra target parameters.
+                with autograd.record():
+                    with autograd.train_mode():
+                        # Returns four nd arrays, each with BATCH_SIZE number of losses.
+                        obj_loss, center_loss, scale_loss, cls_loss = self.central_server.net(data, gt_bboxes, objectness, center_targets, scale_targets, weights, class_targets)
+                sum_loss = obj_loss + center_loss + scale_loss + cls_loss
+
+                # Store the average of the losses for each data batch.
+                self.obj_losses.append(obj_loss.mean().asscalar())
+                self.center_losses.append(center_loss.mean().asscalar())
+                self.scale_losses.append(scale_loss.mean().asscalar())
+                self.cls_losses.append(cls_loss.mean().asscalar())
+                self.sum_losses.append(sum_loss.mean().asscalar())
+            else:
+                outputs = self.central_server.net(data)
+                self.epoch_loss.update(label, nd.softmax(outputs))
+        end = time.time()
+        print('time it takes to calculate loss for 10 validation data', end-start)
 
     def print_accuracy(self, time):
         self.epoch_accuracy.reset()
-        self.epoch_loss.reset()
+        if cfg['dataset'] == 'pascalvoc':
+            self.obj_losses = []
+            self.center_losses = []
+            self.scale_losses = []
+            self.cls_losses = []
+            self.sum_losses = []
+        else:
+            self.epoch_loss.reset()
         print("finding accu and loss ...")
 
         # Calculate accuracy and loss
         self.get_accu_loss()
 
+        # Retrieve acuracy and loss and then save them into a csv.
         _, accu = self.epoch_accuracy.get()
-        _, loss = self.epoch_loss.get()
 
-        # Save accuracy and loss to csv
-        self.save_data(accu, loss, time)
+        if cfg['dataset'] == 'pascalvoc':
+            obj_loss = np.array(self.obj_losses).mean()
+            center_loss = np.array(self.center_losses).mean()
+            scale_loss = np.array(self.scale_losses).mean()
+            cls_loss = np.array(self.cls_losses).mean()
+            sum_loss = np.array(self.sum_losses).mean()
 
-        print("Epoch {:03d}: Loss: {:03f}, Accuracy: {:03f}\n".format(self.num_epoch,
-                                                                    loss,
-                                                                    accu))
+            self.save_data(accu, sum_loss, time, obj_loss, center_loss, scale_loss, cls_loss)
+            print(
+                "Epoch {:03d}: Loss: {:03f}, Accuracy: {:03f}, Object Loss: {:03f}, Center Loss: {:03f}, Scale Loss: {:03f}, CLS Loss: {:03f}\n".format(
+                    self.num_epoch,
+                    sum_loss,
+                    accu, obj_loss, center_loss, scale_loss, cls_loss))
+        else:
+            _, loss = self.epoch_loss.get()
+            self.save_data(accu, loss, time)
+            print("Epoch {:03d}: Loss: {:03f}, Accuracy: {:03f}\n".format(self.num_epoch,
+                                                                          loss,
+                                                                          accu))
 
-    def save_data(self, accu, loss, time):
+    def save_data(self, accu, loss, time, *losses):
         if not os.path.exists('collected_results'):
             os.makedirs('collected_results')
-        dir_name = cfg['dataset'] + '-' + 'VC' + str(cfg['simulation']['num_vc']) + '-' + 'round' + str(self.num_round) + '.csv'
+        dir_name = cfg['dataset'] + '-' + 'VC' + str(cfg['simulation']['num_vc']) + '-' + 'round' + str(
+            self.num_round) + '.csv'
         p = os.path.join('collected_results', dir_name)
+
         with open(p, mode='a') as f:
             writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            writer.writerow([self.num_epoch, time, accu, loss, cfg['aggregation_method'], cfg['attack']])
-            
+            if cfg['dataset'] == 'pascalvoc':
+                if self.num_epoch == 1:
+                    writer.writerow(['Epoch number', 'Time', 'Accuracy', 'Loss', 'Object Loss', 'Center Loss', 'Scale Loss', 'CLS Loss', 'Aggregation Method', 'Attack Type'])
+                writer.writerow([self.num_epoch, time, accu, loss, losses[0], losses[1], losses[2], losses[3], cfg['aggregation_method'], cfg['attack']])
+            else:
+                writer.writerow([self.num_epoch, time, accu, loss, cfg['aggregation_method'], cfg['attack']])
 
     def new_epoch(self):
         self.num_epoch += 1
         # for i, (data, label) in enumerate(self.training_set):
         #     self.training_data.append((data, label))
         self.training_data_byclass, self.training_label_byclass = data_for_polygon(self.polygons)
-
