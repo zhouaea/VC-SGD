5d102136270578d1cb725413d20ce82abc7ff3cf
Neo Zhou <zhouaea@bc.edu>
2021-06-01 16:06:19 -0400

    update helper shell code to create venv

--- version0/download_helper_scripts/install.sh
+++ version0/download_helper_scripts/install.sh
@@ -1,7 +1,6 @@
-$ sudo add-apt-repository ppa:deadsnakes/ppa # Install python 3.9
-$ sudo apt update
-$ sudo apt install python3.9
-sudo apt install python3-pip # Install pip3
-pip3 --version
-sudo pip3 install --upgrade pip
-pip3 install -r requirements.txt # Install dependencies
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.8
+sudo apt-get install python3.8-venv
+python3.8 -m venv env
+pip install -r requirements.txt # Install dependencies
