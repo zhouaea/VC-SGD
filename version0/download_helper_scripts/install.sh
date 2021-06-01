ff4b3d9fd32ff97aa00c0be11b378159dc4ef048
Neo Zhou <zhouaea@bc.edu>
2021-06-01 15:48:34 -0400

    update helper shell code

--- version0/download_helper_scripts/install.sh
+++ version0/download_helper_scripts/install.sh
@@ -1,5 +1,7 @@
-sudo apt update
-sudo apt install python3-pip
+$ sudo add-apt-repository ppa:deadsnakes/ppa # Install python 3.9
+$ sudo apt update
+$ sudo apt install python3.9
+sudo apt install python3-pip # Install pip3
 pip3 --version
 sudo pip3 install --upgrade pip
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt # Install dependencies
