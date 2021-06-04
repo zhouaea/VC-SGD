e815accfc401121ed976ca17100cfb1bcfb255f8
Neo Zhou <zhouaea@bc.edu>
2021-06-04 12:30:46 -0400

    Add checksum feature when downloading pascalvoc

--- version0/download_helper_scripts/download_pascal_voc.py
+++ version0/download_helper_scripts/download_pascal_voc.py
@@ -9,15 +9,19 @@ from gluoncv.utils import download, makedirs
 
 def download_voc(path):
     _DOWNLOAD_URLS = [
-        'http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar',
-        'http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar',
-        'http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar']
+        ('http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar',
+         '34ed68851bce2a36e2a223fa52c661d592c66b3c'),
+        ('http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar',
+         '41a8d6e12baa5ab18ee7f8f8029b9e11805b4ef1'),
+        ('http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar',
+         '4e443f8a2eca6b1dac8a6c57641b67dd40621a49')]
     makedirs(path)
-    for url in _DOWNLOAD_URLS:
-        filename = download(url, path=path)
+    for url, checksum in _DOWNLOAD_URLS:
+        filename = download(url, path=path, sha1_hash=checksum)
         # extract
         with tarfile.open(filename) as tar:
             tar.extractall(path=path)
+
 
 if __name__ == '__main__':
     path = os.path.abspath('../../data/pascalvoc/')
