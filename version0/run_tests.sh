d47fb735b1b0a991cf092731090a4190084c8e58
Neo Zhou <zhouaea@bc.edu>
2021-06-09 15:42:59 -0400

    Add filprofiler to display RAM results after crashing

--- version0/run_tests.sh
+++ version0/run_tests.sh
@@ -1,12 +1,12 @@
 sed -i 's/^num_training_data:.*/num_training_data: 1000/' config.yml
 sed -i 's/^  batch_size:.*/  batch_size: 100/' config.yml
-python3.9 main.py
+python3.9 python -m filprofiler run main.py
 
 sed -i 's/^  batch_size:.*/  batch_size: 50/' config.yml
-python3.9 main.py
+python3.9 python -m filprofiler run main.py
 
 sed -i 's/^  batch_size:.*/  batch_size: 25/' config.yml
-python3.9 main.py
+python3.9 python -m filprofiler run main.py
 
 sed -i 's/^  batch_size:.*/  batch_size: 10/' config.yml
-python3.9 main.py
+python3.9 python -m filprofiler run main.py
