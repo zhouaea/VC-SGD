8ba1ebf528096d4bb1c4f5e62a6310cdfd823360
Neo Zhou <zhouaea@bc.edu>
2021-06-29 13:10:34 -0400

    modify stat analyzer for communication analysis purposes

--- version0/collected_results/stat_analyzer.py
+++ version0/collected_results/stat_analyzer.py
@@ -1,4 +1,3 @@
-import csv
 import numpy as np
 import yaml
 
@@ -9,10 +8,11 @@ runtime_writing_enabled = cfg['write_runtime_statistics']
 
 runtimes = []
 
-runtime_filenames = ['time_to_upload_and_receive_gradients', 'gradient_sizes', 'time_to_decode_gradients', 'time_to_print_gradients', 'time_to_encode_gradients', ]
+runtime_filenames = ['time_to_upload_and_receive_gradients', 'time_to_decode_gradients', 'time_to_print_gradients', 'time_to_encode_gradients', ]
 
 if runtime_writing_enabled is True:
     for filename in runtime_filenames:
+        runtimes = []
         # Store the runtime in each line as a float in an array to be calculated on.
         with open(filename) as log:
             line = log.readline()
@@ -22,3 +22,19 @@ if runtime_writing_enabled is True:
 
         print("Median and average runtime in seconds for " + filename + " :")
         print(str(np.percentile(runtimes, 50)) + ", " + str(np.average(runtimes)))
+
+
+gradient_sizes = []
+if runtime_writing_enabled is True:
+    # Store the runtime in each line as a float in an array to be calculated on.
+    with open('gradient_sizes') as log:
+        line = log.readline()
+        while line != '':
+            runtimes.append(float(line))
+            line = log.readline()
+
+            print("Median and average gradient size in bytes:")
+            print(str(np.percentile(runtimes, 50)) + ", " + str(np.average(runtimes)))
+
+            print("Number of gradients delivered:")
+            print(len(gradient_sizes))
