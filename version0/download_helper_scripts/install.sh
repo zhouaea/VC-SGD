30abfe74f086b00aef82aa76523f197f78965f87
Neo Zhou <zhouaea@bc.edu>
2021-09-14 10:57:42 -0400

    modify installation script for bc machine

--- version0/download_helper_scripts/install.sh
+++ version0/download_helper_scripts/install.sh
@@ -6,22 +6,23 @@
 #   cd version0/download_helper_scripts/
 #   . install.sh
 
+# Make sure this is run in the download_helper_scripts directory.
 
 # Download Python 3.9
-sudo apt update
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install python3.9
+apt update
+apt install software-properties-common
+add-apt-repository ppa:deadsnakes/ppa
+apt update
+apt install python3.9
 python3.9 --version
 
 # Download pip for python 3.9
-sudo apt install python3.9-distutils
+apt install python3.9-distutils
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.9 get-pip.py
 
 # Install dependencies
-sudo python3.9 -m pip install -t /usr/local/cuda-11.2/python_packages --cache-dir=/usr/local/cuda-11.2/tmp -r requirements.txt
+python3.9 -m pip install requirements.txt
 
 # Install pascalvoc dataset in VC-SGD/data/pascalvoc
 python3.9 download_pascal_voc.py
@@ -29,4 +30,4 @@ rm ../../data/pascalvoc/*.tar
 cd ..
 
 # Download application that will continue to run experiments even if the ssh connection breaks.
-sudo apt-get install screen
+#apt-get install screen
