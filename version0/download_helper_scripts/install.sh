52aeb7b13c0a226bace2e177d755ce8857418da1
Neo Zhou <zhouaea@bc.edu>
2021-09-14 11:00:10 -0400

    fix installation script

--- version0/download_helper_scripts/install.sh
+++ version0/download_helper_scripts/install.sh
@@ -1,4 +1,4 @@
-# For installation on a VM with Ubuntu 20.04 and Python 3.5
+# For installation on a BC VM with Ubuntu 28.04 and Python 3.6
 # Copy paste this:
 #   git clone https://github.com/zhouaea/VC-SGD.git
 #   cd VC-SGD/
@@ -8,24 +8,11 @@
 
 # Make sure this is run in the download_helper_scripts directory.
 
-# Download Python 3.9
-apt update
-apt install software-properties-common
-add-apt-repository ppa:deadsnakes/ppa
-apt update
-apt install python3.9
-python3.9 --version
-
-# Download pip for python 3.9
-apt install python3.9-distutils
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.9 get-pip.py
-
 # Install dependencies
-python3.9 -m pip install requirements.txt
+python3 -m pip install requirements.txt
 
 # Install pascalvoc dataset in VC-SGD/data/pascalvoc
-python3.9 download_pascal_voc.py
+python3 download_pascal_voc.py
 rm ../../data/pascalvoc/*.tar
 cd ..
 
